d8c2e7652298f98129ce8c6b2accc196c384a7e4
evg50 <f1.evgeniy4@gmail.com>
2024-05-19 19:45:28 +0200

    finily1

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,31 @@
+// (() => {
+//     const refs = {
+//       openMenuBtn: document.querySelector('[data-menu-open]'),
+//       closeMenuBtn: document.querySelector('[data-menu-close]'),
+//       menu: document.querySelector('[data-menu]'),
+//     };
+
+//     refs.openMenuBtn.addEventListener('click', toggleModal);
+//     refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//       document.body.classList.toggle("is-open");
+//       refs.menu.classList.toggle('is-close');
+//     }
+//   })();
+
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('[data-menu-open]'),
-      closeMenuBtn: document.querySelector('[data-menu-close]'),
-      menu: document.querySelector('[data-menu]'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleModal);
-    refs.closeMenuBtn.addEventListener('click', toggleModal);
-  
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
   function toggleModal() {
-      document.body.classList.toggle("is-open");
-      refs.menu.classList.toggle('is-close');
-    }
-  })();
+    document.body.classList.toggle('menu-open');
+    refs.menu.classList.toggle('is-open');
+  }
+})();

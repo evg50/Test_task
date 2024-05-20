21c0aef7103d5d9b51eaefabba2a9ea8c983842a
evg50 <f1.evgeniy4@gmail.com>
2024-05-20 14:13:20 +0200

    finaly 2

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,3 @@
-// (() => {
-//     const refs = {
-//       openMenuBtn: document.querySelector('[data-menu-open]'),
-//       closeMenuBtn: document.querySelector('[data-menu-close]'),
-//       menu: document.querySelector('[data-menu]'),
-//     };
-
-//     refs.openMenuBtn.addEventListener('click', toggleModal);
-//     refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//       document.body.classList.toggle("is-open");
-//       refs.menu.classList.toggle('is-close');
-//     }
-//   })();
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
@@ -25,7 +9,7 @@
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('menu-open');
-    refs.menu.classList.toggle('is-open');
+    document.body.classList.toggle('is-open');
+    refs.menu.classList.toggle('is-close');
   }
 })();
